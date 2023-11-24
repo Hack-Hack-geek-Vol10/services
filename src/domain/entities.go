8cca956145311e275b0de86b53207cd7cfd0632a
murasame29 <oogiriminister@gmail.com>
2023-11-24 16:06:27 +0900

    add: db entities

--- src/domain/entities.go
+++ src/domain/entities.go
@@ -15,3 +15,17 @@ type User struct {
 	Email  string `json:"email"`
 	Name   string `json:"name"`
 }
+
+type ProjectMember struct {
+	ProjectID string    `json:"project_id"`
+	UserID    string    `json:"user_id"`
+	Authority Authority `json:"authority"`
+}
+
+type Authority string
+
+const (
+	AuthorityOwner Authority = "owner"
+	ReadAndWrite   Authority = "read_and_write"
+	ReadOnly       Authority = "read_only"
+)
