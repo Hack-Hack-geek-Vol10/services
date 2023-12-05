bd203b2e037da1dba2b246085601750b4e1cc069
murasame <oogiriminister@gmail.com>
2023-12-05 17:34:07 +0900

    add:lexer template

---
+++ scml/go.mod
@@ -0,0 +1,3 @@
+module github.com/schema-creator/services/scml
+
+go 1.21.4
---
+++ scml/lexer/lexer.go
@@ -0,0 +1,132 @@
+package lexer
+
+import (
+	"github.com/schema-creator/services/scml/token"
+)
+
+// Lexer は現在の文字やその位置などを格納するtype
+type Lexer struct {
+	input        string
+	position     int  // 入力における現在の位置（現在の文字を指し示す）
+	readPosition int  // これから読み込む位置（現在の文字の次）
+	ch           byte // 現在検査中の文字
+}
+
+// New はinputをもとにLexerを生成し、一文字読み取ったLexerを返す
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// NextToken は現在検査中の文字をtoken.Tokenとして返す
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhitespace()
+
+	switch l.ch {
+
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	case '[':
+		tok = newToken(token.LBRACKET, l.ch)
+	case ']':
+		tok = newToken(token.RBRACKET, l.ch)
+	case ':':
+		tok = newToken(token.COLON, l.ch)
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		}
+		tok = newToken(token.ILLEGAL, l.ch)
+	}
+	l.readChar()
+	return tok
+}
+
+// 英文字 + '_' であればtrue、それ以外はfalse
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+// peekChar は先読みし、読み込み位置を表すreadPositionは+1しない
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		// 入力が終端に達している場合、ファイルの終わりを示すために"NULL"
+		// を表す0を設定する
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		// 入力が終端に達している場合、ファイルの終わりを示すために"NULL"
+		// を表す0を設定する
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+// 英字 + '_' 以外の文字が出てくるまで読み込み、識別子/キーワードとして返す
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// 数字以外の文字が出てくるまで読み込み、数値として返す
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// skipWhitespace は空白文字が続く間、文字を1文字ずつ読ませて
+// newTokenを呼び出さないことで、Token検出処理をスキップする
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+// readString は閉じ二重引用符か入力の最後に至るまでreadCharを読んで
+// そこまでの文字列を返す
+func (l *Lexer) readString() string {
+	position := l.position + 1
+	for {
+		l.readChar()
+		if l.ch == '"' || l.ch == 0 {
+			break
+		}
+	}
+	return l.input[position:l.position]
+}
