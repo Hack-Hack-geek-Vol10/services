cea0958b56cfab8ebcf64bde074a703eb914580d
sikigasa <km3t1114@yahoo.co.jp>
2023-12-12 00:35:27 +0900

    add: saves migrate

---
+++ migrate-service/migrations/000005_saves.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "saves" (
+  "save_id" varchar PRIMARY KEY,
+  "project_id" varchar NOT NULL,
+  "editor" varchar NOT NULL,
+  "object" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT 'now()'
+);
+ALTER TABLE "saves"
+ADD FOREIGN KEY ("project_id") REFERENCES "projects" ("project_id");
