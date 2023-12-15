539a4e8912776e7a796a28527ee4b239c0ccae9d
sikigasa <km3t1114@yahoo.co.jp>
2023-12-16 04:42:54 +0900

    [WIP]

--- sql-service/internal/project/interfaces/http/rotuer.go
+++ sql-service/internal/project/interfaces/http/rotuer.go
@@ -21,14 +21,6 @@ func NewProjectRouter(gin *gin.Engine, l logger.Logger, pr repository.ProjectRep
 
 	projectApplication := application.NewProjectApplication(l, pr)
 
-	router.Gin.GET("/users/:user_id/projects", projectApplication.GetProjectsByUserID)
-	router.Gin.POST("/users/:user_id/projects", projectApplication.CreateProject)
-	router.Gin.DELETE("/users/:user_id/projects", projectApplication.DeleteAllByUserID)
-
-	router.Gin.GET("/users/:user_id/projects/:project_id", projectApplication.GetProject)
-	router.Gin.PUT("/users/:user_id/projects/:project_id", projectApplication.UpdateProject)
-	router.Gin.PATCH("/users/:user_id/projects/:project_id", projectApplication.JoinProject)
-	router.Gin.DELETE("/users/:user_id/projects/:project_id", projectApplication.DeleteProject)
 	router.Gin.GET("/users/:user_id/projects/:project_id/ddl", projectApplication.ConvertDDL)
 
 	return router.Gin
